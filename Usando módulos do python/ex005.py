162f25ac0d9aa90a4f1f88efc34d64b942b5b232
Hentony Alves <hentony2015@outlook.com>
2020-04-01 18:31:41 -0300

    Create ex005.py

---
+++ Usando módulos do python/ex005.py
@@ -0,0 +1,12 @@
+#O mesmo professor do exercício 004 quer sortear a ordem de apresentação de trabalhos dos alunos.
+#Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
+
+from random import shuffle
+a1 = input('Primeiro aluno: ')
+a2 = input('Segundo aluno: ')
+a3 = input('Terceiro aluno: ')
+a4 = input('Quarto aluno: ')
+alunos = [a1,a2,a3,a4]
+shuffle(alunos)
+print('A ordem de apresentação será:\n{}'.format(alunos))
+
