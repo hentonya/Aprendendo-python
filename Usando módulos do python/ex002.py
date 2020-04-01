3faf8dd3620793428fc7cf61c815942d6ac89333
Hentony Alves <hentony2015@outlook.com>
2020-04-01 16:23:26 -0300

    Resolução do exercício 002

---
+++ Usando módulos do python/ex002.py
@@ -0,0 +1,6 @@
+#Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo retângulo.
+#Calcule e mostre o comprimento da hipotenusa.
+from math import hypot
+o = float(input('Comprimento do cateto oposto: '))
+a = float(input('Comprimento do cateto adjacente '))
+print('O valor da hipotenusa é {:.2f}'.format(hypot(o,a)))
