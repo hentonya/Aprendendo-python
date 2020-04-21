45bb135b4f6eca88faae1685d9b7d703578e3714
Hentony Alves <hentony2015@outlook.com>
2020-04-21 15:51:40 -0300

    Resoluções

--- Repetições em python/ex023.py
+++ Repetições em python/ex023.py
@@ -1,2 +1,21 @@
 # Faça um programa que jogue par ou ímpar com o computador. O jogo só será interrompido quando o jogador perder, mostrando o total de vitórias 
 # consecutivas que ele conquistou no final do jogo. 
+from random import randint
+vitorias = 0 
+while True:
+    jogador = int(input('Digite um número: '))
+    escolha = str(input('Par ou ímpar ? [P/I] ')).strip().lower()[0]
+    if escolha == 'í':
+        escolha = 'i'
+    computador = randint(1,10)
+    print(f'Você jogou {jogador} e o computador jogou {computador}\nO total é {jogador + computador}')
+    if (jogador + computador) % 2 == 0:
+        resultado = 'p'
+    else:
+        resultado = 'i'
+    if resultado == escolha:
+        vitorias += 1 
+        print('\nPARABÉNS !, você venceu\nVamos jogar novamente')
+    else:
+        break
+print(f'GAME OVER\nVocê ganhou {vitorias} vezes')
