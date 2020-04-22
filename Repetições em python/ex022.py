7ba415cb17775e77b7229782f84be9d96b97d195
Hentony Alves <hentony2015@outlook.com>
2020-04-22 15:31:34 -0300

    Update ex022.py

--- Repetições em python/ex022.py
+++ Repetições em python/ex022.py
@@ -1,6 +1,5 @@
 # Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor digitado pelo usuário. O programa será interrompido 
 # quando o número solicitado for negativo
-num = 0 
 while True:
     num = int(input('\nDeseja ver a tabuada de multiplicação de qual número ? '))
     if num < 0:
