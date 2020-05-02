bad18d0f00a8f3d0ddc6ed3801fde7d8e11626eb
Hentony Alves <hentony2015@outlook.com>
2020-05-02 16:29:05 -0300

    Usando o método sort

--- Coleções em python/Listas em python/ex002.py
+++ Coleções em python/Listas em python/ex002.py
@@ -3,3 +3,17 @@ Crie um programa onde o usuário possa digitar vários valores numéricos e cada
 Caso o número já exista lá dentro, ele não será adicionado. No final, serão exibidos todos os 
 valores únicos digitados, em ordem crescente. 
 """
+numbers = list()
+while True:
+    num = int(input('Digite um número: '))
+    if num not in numbers:
+        numbers.append(num)
+    else:
+        print('Esse número já foi digitado, não irei armazená-lo')
+    escolha = ' '
+    while escolha not in 'sn':
+        escolha = str(input('Deseja adicionar mais números ? [S/N] ')).strip().lower()[0]
+    if escolha == 'n':
+        break
+numbers.sort()
+print(f'Números digitados em ordem crescente: {numbers}')
