44eb7040b83394aabaa37182850099a71f065693
Hentony Alves <hentony2015@outlook.com>
2020-05-06 20:25:25 -0300

    Create ex009.py

---
+++ Coleções em python/Listas em python/ex009.py
@@ -0,0 +1,9 @@
+# Crie um programa que declare uma matriz de dimensão 3x3 e preencha com valores lidos pelo teclado. 
+# No final, mostre a matriz na tela, com a formatação correta.
+matriz = [[],[],[]]
+for linha in range (0,3):
+    for coluna in range (0,3):
+        matriz[linha].append(int(input(f'Digite um valor para [{linha},{coluna}]: ')))
+for linha in matriz:
+    print(linha)
+     
