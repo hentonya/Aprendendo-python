a6daf9b782794cddb66961db062a0bf77afea874
Hentony Alves <hentony2015@outlook.com>
2020-05-07 16:26:59 -0300

    Matriz em python

--- Coleções em python/Listas em python/ex009.py
+++ Coleções em python/Listas em python/ex009.py
@@ -4,6 +4,8 @@ matriz = [[],[],[]]
 for linha in range (0,3):
     for coluna in range (0,3):
         matriz[linha].append(int(input(f'Digite um valor para [{linha},{coluna}]: ')))
-for linha in matriz:
-    print(linha)
+for linha in range (0,3):
+    for coluna in range (0,3):
+        print(f'[{matriz[linha][coluna]:^5}]', end='')
+    print()
      
