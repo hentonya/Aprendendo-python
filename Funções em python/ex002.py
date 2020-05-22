a0cb92b2ed2d10e7acb42ce4051b4e6671975004
Hentony Alves <hentony2015@outlook.com>
2020-05-22 14:31:55 -0300

    Update ex002.py

--- Funções em python/ex002.py
+++ Funções em python/ex002.py
@@ -2,6 +2,6 @@
 # qualquer como parâmetro e mostre uma mensagem com tamanho adaptável.
 def escreva(txt):
     print( (len(txt)+4)*'~' )
-    print(f'{txt:^{len(txt)+4}}')
+    print(f'  {txt}')
     print( (len(txt)+4)*'~' )
 escreva(str(input('Digite a mensagem: ')))
