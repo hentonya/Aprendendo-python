6af1e7a81bdd96917fc631f17906fb9b14679b9e
Hentony Alves <hentony2015@outlook.com>
2020-06-09 18:37:29 -0300

    Reduzindo ainda mais o programa

---
+++ Modularização em Python/ex004/teste.py
@@ -0,0 +1,7 @@
+# Adicione no módulo moeda.py criado nos exercícios anteriores, uma função chamada 
+# resumo, que mostre na tela algumas informações geradas pelas funções que já 
+# temos no módulo criado até aqui.
+import moeda
+preço = float(input('Digite o preço: R$'))
+moeda.resumo(preço)
+ 
